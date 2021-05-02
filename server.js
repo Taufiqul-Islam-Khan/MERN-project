c9aa077a65ad121b0c8a5045d64d6c6741eeaa29
Taufiqul-Islam-Khan <taufiqulislamkhan@gmail.com>
2021-05-03 00:00:24 +0600

    database connected successfully

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ connectDB();
 
 app.get ('/',(req,res) => res.send('API Running'));
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
