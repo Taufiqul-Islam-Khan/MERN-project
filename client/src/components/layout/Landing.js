4b8745f287a68ddd812dd5f4b276abbfd1fab913
Taufiqul-Islam-Khan <taufiqulislamkhan@gmail.com>
2021-05-19 02:10:57 +0600

    done Alert Reducer, Action _ Types

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 export const Landing = () => {
-    return (
-        <section className="landing">
-      <div className="dark-overlay">
-        <div class="landing-inner">
-          <h1 class="x-large">Developer Connector</h1>
-          <p class="lead">
-            Hello Developers!!!
-          </p>
-          <p class="lead2">
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div class='landing-inner'>
+          <h1 class='x-large'>Developer Connector</h1>
+          <p class='lead'>Hello Developers!!!</p>
+          <p class='lead2'>
             Create your own profile/portfolio, share posts and get help from
             other developers
           </p>
-          <div class="buttons">
-            <Link to="/register" class="btn btn-primary">Sign Up</Link>
-            <Link to="/login" class="btn btn-light">Login</Link>
+          <div class='buttons'>
+            <Link to='/register' class='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' class='btn btn-light'>
+              Login
+            </Link>
           </div>
         </div>
       </div>
     </section>
-    )
-}
+  );
+};
 
-export default Landing
+export default Landing;
